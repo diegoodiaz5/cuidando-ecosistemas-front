74365933bcf39de6ca397a313aa0827c71da401d
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-14 14:07:23 -0300

    Input update + useNavigate

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,44 @@
 import React from "react";
 import "./Home.css"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+
+    const navigate = useNavigate();
+
+    const [valueInput, setValueInput] = useState('');
+
+    const handleChange = e => {
+        setValueInput(e.target.value);
+    }
+
+    useEffect (() => {
+        const close = document.getElementById("closeButton");
+        const inputSearch = document.getElementById("inputSearch");
+        if (valueInput !== '') {
+            close.style.display = "inline";
+            inputSearch.style.width = "80%";
+        } else{
+            close.style.display = "none";
+            inputSearch.style.width ="97%"
+        };
+    })
+
+    const clearInput = () => {
+        setValueInput('');
+    }
+
     return (
         <div id="home">
             <div className="arrowAndAccountBox">
-                <img src={require('../Images/leftArrow.png')} alt="leftArrow" width="11.78px" height="20px" className="leftArrow"/>
+                <img src={require('../Images/leftArrow.png')} alt="leftArrow" width="11.78px" height="20px" className="leftArrow" onClick={()=>navigate(-1)}/>
                 <img src={require('../Images/userAccountBox.png')} alt="userAccountBox" width="20px" height="20px" className="userAccountBox"/>
             </div>
-            <input type="text" className="inputSearch" placeholder="Search..."/>
+            <div className="inputConteiner">
+                <input type="text" id="inputSearch" placeholder="Search..." value={valueInput} onChange={handleChange}/>
+                <img src={require('../Images/close.png')} alt="closeButton" id="closeButton" onClick={clearInput}/>
+            </div>
             <div className="container">
                 <div className="box">
                     <img src={require('../Images/clock.png')} alt="clock" width="32px"/>
