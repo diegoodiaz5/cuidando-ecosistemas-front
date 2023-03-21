c5bf24b1dea37f160e6a27ba9ca736d7a27f17d3
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-21 09:09:46 -0300

    Added Routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import User from "./Components/User/User.jsx"
 import Home from "./Components/Home/Home.jsx"
+import MyPlant from "./Components/MyPlant/MyPlant.jsx"
+import Forum from "./Components/Forum/Forum.jsx"
+import UserConfiguration from './Components/UserConfiguration/UserConfiguration';
+import AddPlant from './Components/AddPlant/AddPlant';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<User/>}/>
-        <Route path="Home" element={<Home/>} />
+        <Route path="/" element={<User />} />
+        <Route path="home" element={<Home />} />
+        <Route path="myplant" element={<MyPlant />} />
+        <Route path="forum" element={<Forum />} />
+        <Route path="userconfiguration" element={<UserConfiguration />} />
+        <Route path="addplant" element={<AddPlant />} />
       </Routes>
     </BrowserRouter>
   );
