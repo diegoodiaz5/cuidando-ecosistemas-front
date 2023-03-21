0794339c977abecf80c8c06906306bbc1e845b15
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-21 09:09:34 -0300

    Added useNavigate

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,20 @@
 import React, { useEffect } from "react";
 import "./NavBar.css";
 import highlight from "../../Functions/Highlight";
-
+import { useNavigate } from "react-router-dom";
 
 export default function NavBar() {
+    const navigate = useNavigate();
     useEffect(() => {
-       highlight();
-    }) 
+        highlight();
+    })
 
-    return(
+    return (
         <div className="navBarConteiner">
-            <img src={require('../Images/home.png')} alt="homeIcon" className="iconsNavBar"/>
-            <img src={require('../Images/message.png')} alt="messageIcon" className="iconsNavBar"/>
-            <img src={require('../Images/plus.png')} alt="plusIcon" className="iconsNavBar"/>
-            <img src={require('../Images/flower.png')} alt="flowerIcon" className="iconsNavBar"/>
+            <img src={require('../Images/home.png')} alt="homeIcon" className="iconsNavBar" onClick={() => navigate("/home")} />
+            <img src={require('../Images/message.png')} alt="messageIcon" className="iconsNavBar" onClick={() => navigate("/forum")} />
+            <img src={require('../Images/plus.png')} alt="plusIcon" className="iconsNavBar" onClick={() => navigate("/addplant")} />
+            <img src={require('../Images/flower.png')} alt="flowerIcon" className="iconsNavBar" onClick={() => navigate("/myplant")} />
         </div>
     )
 }
