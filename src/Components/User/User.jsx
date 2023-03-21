c85f7da070aa9602b3b463c87deb30a15ac7be4e
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-21 09:10:29 -0300

    Edited User page

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,27 +1,49 @@
 import React from "react"
 import "./User.css"
-import { useNavigate } from "react-router-dom";
 export default function User() {
-    const navigate=useNavigate( console.log("ola"))
-    return (
-        <main onClick={()=>navigate(+1)}>
-            <div id="user_main_container" >
-                <div id="logo_front_container">
-         
-                <div id="logo_contain">
 
-            <img  src={require('../Images/logo.png')} alt="logo.png" id="logo"  />
-                </div>
-            <div id="title_contain">
-            <h3>CARING </h3>
-            <h4>FOR</h4>
-            <h5>ECOSYSTEMS</h5>
+    const showLogIn = () => {
+        document.getElementById("firstScreen").style.display = "none";
+        document.getElementById("logInScreen").style.display = "flex";
+        document.getElementById("registerContainer").style.display = "none";
+    }
+
+    const showRegister = () => {
+        document.getElementById("logInScreen").style.display = "none";
+        document.getElementById("registerContainer").style.display = "flex";
+    }
+
+    return (
+        <div id="userContainer">
+            <div id="firstScreen" onClick={showLogIn}>
+                <img src={require('../Images/logo.png')} alt="logo" id="logo" />
+                <h3 id="caringText">CUIDANDO</h3>
+                <h4 id="ecosystemsText">ECOSISTEMAS</h4>
+                <h1 id="hiTitle">¡Hi!</h1>
             </div>
+            <div id="logInScreen">
+                <img src={require('../Images/logo.png')} alt="logo" id="logo" />
+                <h3 id="caringText">CUIDANDO</h3>
+                <h4 id="ecosystemsText">ECOSISTEMAS</h4>
+
+                <input type="text" placeholder="User" className="inputUserScreen" id="inputUser" />
+                <input type="text" placeholder="Password" className="inputUserScreen" />
+
+                <button className="buttonUserScreen">Sign in</button>
+                <button className="buttonUserScreen" onClick={showRegister}>Register</button>
+
+                <button className="buttonSocialNetwork">Sign in with Google</button>
+                <button className="buttonSocialNetwork">Sign in with Facebook</button>
+                <button className="buttonSocialNetwork">Sign in with ICloud</button>
             </div>
+            <div id="registerContainer">
+                <img src={require('../Images/leftArrow.png')} alt="arrowBack" onClick={showLogIn} id="arrowBackRegister" />
+                <input type="text" placeholder="User" className="inputUserScreen" id="firstInputRegister" />
+                <input type="email" placeholder="Email" className="inputUserScreen" />
+                <input type="password" placeholder="Password" className="inputUserScreen" />
+                <button className="buttonUserScreen" onClick={showLogIn} id="signInButton">Sign In</button>
+                <button className="buttonUserScreen">Register</button>
             </div>
-            <div id="hi_container">
-            <h1>¡Hi!</h1>
-            </div>
-        </main>
+        </div >
     )
 }
