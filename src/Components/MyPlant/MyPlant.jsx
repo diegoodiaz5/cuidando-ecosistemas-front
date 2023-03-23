70aa18560b610a3e1aff0b9034819cc86bcaf9a6
Agustin Martinez <agustinmcuello@gmail.com>
2023-03-23 13:35:11 -0300

    myplants-added

--- src/Components/MyPlant/MyPlant.jsx
+++ src/Components/MyPlant/MyPlant.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import "./MyPlant.css"
-import NavBar from '../NavBar/NavBar'
-
+import React from "react";
+import "./MyPlant.css";
+import NavBar from "../NavBar/NavBar";
+import florist from "../Images/florist.png";
+import more from "../Images/More.png";
 export default function MyPlant() {
-    return (
-        <>
-            <NavBar />
-        </>
-    )
+  return (
+    <>
+      <div className="myplants-container">
+        <h2>
+          <section id="myplants-title">
+            <p>
+              My Plants
+              <img id="florist" src={florist} alt="florist" />
+            </p>
+            <img src={more} alt="more" />
+          </section>
+        </h2>
+        <section className="plants-list">
+            
+        </section>
+      </div>
+      <NavBar />
+    </>
+  );
 }
