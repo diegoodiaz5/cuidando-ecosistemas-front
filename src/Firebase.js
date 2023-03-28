0f8a28e52d77a39f7e7d2085ead4c0b64986fb9f
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-28 12:33:42 -0300

    Added Firebase

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD3FC5t4_tENyGB6U-WEC2IzuDtjFbXpHk",
+    authDomain: "plantas-coding-challenge-69c03.firebaseapp.com",
+    projectId: "plantas-coding-challenge-69c03",
+    storageBucket: "plantas-coding-challenge-69c03.appspot.com",
+    messagingSenderId: "695250842283",
+    appId: "1:695250842283:web:5f508edf7e84f9983d7dee",
+    measurementId: "G-MC938LP8GG"
+};
+
+
+const app = initializeApp(firebaseConfig);
