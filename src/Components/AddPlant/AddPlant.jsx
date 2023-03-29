6ec0f6c2e38714ddd4ff3a90f82601d8c125c6fe
Diego Díaz <diego_diaz9990@hotmail.com>
2023-03-29 08:47:35 -0300

    Edited the component "AddPlant"

--- src/Components/AddPlant/AddPlant.jsx
+++ src/Components/AddPlant/AddPlant.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import "./AddPlant.css"
+import NavBar from "../NavBar/NavBar"
+import { useState } from 'react'
 
 export default function AddPlant() {
+
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [health, setHealth] = useState('');
+    const [information, setInformation] = useState('');
+    const [recomendation, setRecomendation] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    }
+
     return (
         <>
+            <div id="containerAddPlant">
+                <div>
+                    <h2 id="titleAddPlant">Add a plant!</h2>
+                    <img src={require('../Images/plant.png')} alt="iconPlant" id="iconPlant" />
+                </div>
+                <form action="" onSubmit={handleSubmit} id="formAddPlant">
+                    <label>Name:
+                        <input type="text" className="inputAddPlant" onChange={(e) => setName(e.target.value)} />
+                    </label>
+                    <label htmlFor="">Image (url):
+                        <input type="text" className="inputAddPlant" onChange={(e) => setImage(e.target.value)} />
+                    </label>
+                    <label htmlFor="">Health:
+                        <textarea type="text" className="inputAddPlant" onChange={(e) => setHealth(e.target.value)} />
+                    </label>
+                    <label htmlFor="">Information:
+                        <textarea type="text" className="inputAddPlant" onChange={(e) => setInformation(e.target.value)} />
+                    </label>
+                    <label htmlFor="">Recomendation:
+                        <textarea type="text" className="inputAddPlant" onChange={(e) => setRecomendation(e.target.value)} />
+                    </label>
 
+                    <button type='submit' id="addPlantButton">Add!</button>
+                </form>
+            </div>
+            <NavBar />
         </>
     )
 }
