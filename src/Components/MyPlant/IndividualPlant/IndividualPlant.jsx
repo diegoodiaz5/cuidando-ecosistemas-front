f2ff2154b9aef6462487ba6bad720692d99fa455
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-05 11:17:42 -0300

    Created "IndividualPlant" component

---
+++ src/Components/MyPlant/IndividualPlant/IndividualPlant.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./IndividualPlant.css"
+
+export default function IndividualPlant({ plant }) {
+    return (
+        <div className='plantContainer'>
+            <img className='plantImage' src={plant.image} alt='plantImage' width="150px" height="150px" />
+        </div>
+    )
+}
