2075f4214fac34ced6e8e8c3e5a96b954d800527
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-05 11:17:07 -0300

    Created the component MyPlant

--- src/Components/MyPlant/MyPlant.jsx
+++ src/Components/MyPlant/MyPlant.jsx
@@ -1,25 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import "./MyPlant.css";
 import NavBar from "../NavBar/NavBar";
-import florist from "../Images/florist.png";
-import more from "../Images/More.png";
+import IndividualPlant from "./IndividualPlant/IndividualPlant"
+
 export default function MyPlant() {
+
+  const [myPlantsList, setMyPlantsList] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [userUid, setUserUid] = useState('');
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      setUserUid(uid);
+    } else {
+      console.log(`User not loged`)
+    }
+  });
+
+  useEffect(() => {
+    const getPlants = async () => {
+      const data = {
+        uid: userUid
+      }
+      const response = await fetch('http://localhost:3001/getPlants', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const responseJson = await response.json();
+      setMyPlantsList(responseJson);
+      setLoading(false);
+    };
+    getPlants();
+  });
+
   return (
     <>
-      <div className="myplants-container">
-        <h2>
-          <section id="myplants-title">
-            <p>
-              My Plants
-              <img id="florist" src={florist} alt="florist" />
-            </p>
-            <img src={more} alt="more" />
-          </section>
-        </h2>
-        <section className="plants-list">
-            
+      <div id="myplantsContainer">
+        <div id="myplantsTitle">
+          <span>
+            <h2>
+              My Plants </h2>
+            <img id="florist" src={require('../Images/florist.png')} alt="florist" />
+          </span>
+          <img src={require('../Images/More.png')} alt="more" />
+        </div>
+        <section id="whiteContainer">
+          {loading ? <img src={require('../Images/loading.gif')} alt="loadingGif" /> :
+
+            <div id="plantsContainer">
+              {myPlantsList.map((plant) => (
+                <IndividualPlant plant={plant} />
+              ))}
+            </div>
+          }
+
+
         </section>
-      </div>
+      </div >
       <NavBar />
     </>
   );
