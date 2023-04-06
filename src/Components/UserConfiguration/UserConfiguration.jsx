f9cb8be40a66e9634e3af9cc5473a7e1f20edd61
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-06 14:43:08 -0300

    Added the ArrowAndLogo component into UserConfiguration

--- src/Components/UserConfiguration/UserConfiguration.jsx
+++ src/Components/UserConfiguration/UserConfiguration.jsx
@@ -1,25 +1,20 @@
 import React from 'react'
 import "./UserConfiguration.css"
-import NavBar from '../NavBar/NavBar'
 import { useNavigate } from "react-router-dom";
+import NavBar from '../NavBar/NavBar'
+import ArrowAndLogo from '../ArrowAndLogo/ArrowAndLogo';
 
 export default function UserConfiguration() {
-
     const navigate = useNavigate();
-
     return (
         <>
             <section id='sectionUserConfiguration'>
-                <div id='firstLineConfiguration'>
-                    <img src={require('../Images/leftArrow.png')} alt="leftArrow" id='leftArrowConfiguration' />
-                    <img src={require('../Images/logo.png')} alt="logo" />
-                </div>
+                <ArrowAndLogo />
                 <section id='whiteContainerConfiguration'>
                     <h3>Configuration</h3>
                     <button className="configButton" onClick={() => navigate('/userconfiguration/myprofile')}>My Profile</button>
                     <button className="configButton" onClick={() => navigate('/help')}>Help</button>
                     <button className="configButton" onClick={() => navigate('/notifications')}>Notifications</button>
-
                     <p onClick={() => navigate('/termsandconditions')}>Terms and Conditions</p>
                 </section>
             </section>
