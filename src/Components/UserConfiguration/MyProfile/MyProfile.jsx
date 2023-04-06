59e1b4c7b28a15ff04fbb1e1d34f06c2b7bd1bf4
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-06 14:44:41 -0300

    Created "MyProfile" component

--- src/Components/UserConfiguration/MyProfile/MyProfile.jsx
+++ src/Components/UserConfiguration/MyProfile/MyProfile.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './MyProfile.css'
+import ArrowAndLogo from '../../ArrowAndLogo/ArrowAndLogo'
+import NavBar from '../../NavBar/NavBar'
+import ConfigurationBox from '../ConfigurationBox/ConfigurationBox'
 
 export default function MyProfile() {
+    const info = {
+        title: 'My Profile',
+        list: {
+            firstItem: 'Profile picture',
+            secondItem: 'Username',
+            thirdItem: 'Description'
+        }
+    }
     return (
-        <div>MyProfile</div>
+        <>
+            <div className='ContainerConfigs'>
+                <ArrowAndLogo />
+                <ConfigurationBox info={info} />
+            </div>
+            <NavBar />
+        </>
     )
 }
