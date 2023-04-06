e48fcfd4c0e3577ddf2b43adbf9362c40a2b5577
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-06 14:43:54 -0300

    Created the "ConfigurationBox" component

---
+++ src/Components/UserConfiguration/ConfigurationBox/ConfigurationBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ConfigurationBox.css';
+import { useNavigate } from 'react-router-dom';
+
+export default function ConfigurationBox({ info }) {
+    const navigate = useNavigate();
+    return (
+        <div id='whiteContainerConfig'>
+            <h3>Configuration</h3>
+
+            <h2 id='titleConfig'>{info.title}</h2>
+
+            <ul id='listContainer'>
+                <li>{info.list.firstItem}</li>
+                <li>{info.list.secondItem}</li>
+                <li>{info.list.thirdItem}</li>
+            </ul>
+
+            <p id='termsParagraph' onClick={() => navigate("termsandconditions")}>Terms and Conditions</p>
+        </div>
+    )
+}
