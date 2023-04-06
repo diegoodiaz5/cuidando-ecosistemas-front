c78877906a7edc5cf1b3ea9235c30ec738b65264
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-06 12:43:15 -0300

    Routes Created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import MyPlant from "./Components/MyPlant/MyPlant.jsx"
 import Forum from "./Components/Forum/Forum.jsx"
 import UserConfiguration from './Components/UserConfiguration/UserConfiguration';
 import AddPlant from './Components/AddPlant/AddPlant';
+import MyProfile from './Components/UserConfiguration/MyProfile/MyProfile'
+import Help from "./Components/Help/Help";
+import Notifications from "./Components/Notifications/Notifications"
+import TermsAndConditions from "./Components/TermsAndConditions/TermsAndConditions"
 import './Firebase'
 
 function App() {
@@ -18,7 +22,11 @@ function App() {
         <Route path="myplant" element={<MyPlant />} />
         <Route path="forum" element={<Forum />} />
         <Route path="userconfiguration" element={<UserConfiguration />} />
+        <Route path="userconfiguration/myprofile" element={<MyProfile />} />
         <Route path="addplant" element={<AddPlant />} />
+        <Route path="help" element={<Help />} />
+        <Route path="notifications" element={<Notifications />} />
+        <Route path="termsandconditions" element={<TermsAndConditions />} />
       </Routes>
     </BrowserRouter>
   );
