96d4018426ce1fa28fd505bd6935836d37f25a25
Diego Díaz <diego_diaz9990@hotmail.com>
2023-04-08 21:00:55 -0300

    Created "Forum" component

--- src/Components/Forum/Forum.jsx
+++ src/Components/Forum/Forum.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Forum.css"
 import NavBar from '../NavBar/NavBar'
+import ArrowAndLogo from '../ArrowAndLogo/ArrowAndLogo'
 
 export default function Forum() {
     return (
-        <>
+        <section id='forum'>
+            <div id='subConteinerForum'>
+                <ArrowAndLogo />
+                <div id='titleForum'>Forum</div>
+                <div id='shareExperience'>Share your experience here</div>
+            </div>
             <NavBar />
-        </>
+        </section>
     )
 }
