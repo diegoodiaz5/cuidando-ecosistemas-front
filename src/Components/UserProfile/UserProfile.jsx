8db21a4fed847a37c5d899f333bcabfa653c336e
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-07 11:48:49 -0300

    Created the fetch and using the user Props

--- src/Components/UserProfile/UserProfile.jsx
+++ src/Components/UserProfile/UserProfile.jsx
@@ -1,57 +1,73 @@
-import React from 'react'
 import './UserProfile.css'
 import NavBar from '../NavBar/NavBar'
 import ArrowAndLogo from '../ArrowAndLogo/ArrowAndLogo'
+import React, { useEffect } from 'react'
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState } from 'react';
 
 export default function UserProfile() {
 
-  const [user, setUser] = useState({});
-
   const auth = getAuth();
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
+  const [userData, setUserData] = useState({});
+  const [loading, setLoading] = useState(true);
 
-      setUser(user);
-    } else {
-      console.log(`User not loged`)
-    }
-  });
+  useEffect(() => {
+    let uid;
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        uid = user.uid;
+        const getUserInfo = async () => {
+          const res = await fetch(`http://localhost:3001/userId/${uid}`, {
+            headers: {
+              "Content-Type": "application/json"
+            }
+          });
+          const resJson = await res.json();
+          setUserData(resJson);
+          setLoading(false);
+        }
+        getUserInfo();
+      }
+
+    });
+  })
 
   return (
     <>
-      <div id='userProfileContainer'>
-        <ArrowAndLogo />
-        <div id='firstContainerUserProfile'>
-          <img id='profilePicture' src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="profilePicture" />
-          <div id='informationUser'>
-            <h3>Username</h3>
-            <p>Information about the user </p>
+      {loading ? <div>Hola</div> : <section id='userProfileSection'>
+        <div id='userProfileContainer'>
+          <ArrowAndLogo />
+          <div id='firstContainerUserProfile'>
+            <img id='profilePicture' src={require("../Images/avatarDefault.webp")} alt="profilePicture" />
+            <div id='informationUser'>
+              <h3>{userData.information.username}</h3>
+              <p>Information about the user </p>
+            </div>
           </div>
-        </div>
-        <ul id='ulUserProfile'>
-          <li>
-            <p>Plants</p>
-            0
-          </li>
-          <li>
-            <p>Comments</p>
-            0
-          </li>
-          <li>
-            <p>Followers</p>
-            0
-          </li>
-          <li>
-            <p>Following</p>
-            0
-          </li>
-        </ul>
-      </div>
-      <h1>User posts here</h1>
-      <NavBar />
+          <ul id='ulUserProfile'>
+            <li>
+              <p>Plants</p>
+              {userData.information.posts}
+            </li>
+            <li>
+              <p>Comments</p>
+              {userData.information.comments}
+            </li>
+            <li>
+              <p>Followers</p>
+              0
+            </li>
+            <li>
+              <p>Following</p>
+              0
+            </li>
+          </ul>
 
+
+        </div>
+        <h1>User posts here</h1>
+        <NavBar />
+      </section>}
     </>
   )
 }
