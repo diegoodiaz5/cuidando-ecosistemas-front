b943eebbf46c97ce2a08d005780ab00d7911b8d3
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-09 23:28:45 -0300

    Progress in the "IndividualPost" component

--- src/Components/Forum/IndividualPost/IndividualPost.jsx
+++ src/Components/Forum/IndividualPost/IndividualPost.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import './IndividualPost.css'
 
 export default function IndividualPost({ plant }) {
+
     return (
-        <>
-            <div className='photoAndName'><img alt="profilePhoto" /> <p>{plant.name}</p></div>
-            <div><img src={plant.image} className='flowerPhoto' alt="flowerPhoto" /></div>
-            <input className='commentPostInput' type="text" placeholder='Write a comment' />
-        </>
+        <div className='conteinerIndividualPost'>
+            <div className='photoAndName'>
+                <img alt="profilePhoto" src={plant.authorPhoto} className='profilePhotoForum' />
+                <p className='userNameInPostForum'>{plant.authorUsername}</p>
+            </div>
+
+            <img src={plant.image} className='flowerPhoto' alt="flowerPhoto" />
+            <div className='conteinerInputComments'>
+                <input className='commentPostInput' type="text" placeholder='Write a comment...' />
+                <img src={require('../../Images/send.png')} alt='sendIcon' className='sendImage' />
+            </div>
+        </div>
     )
 }
